b875bb717097e970f63d1f56e62707db8c74a0c7
matti909 <matias.saantiago@gmail.com>
2025-03-17 11:08:51 -0300

    path section added

--- app/sections/Path.tsx
+++ app/sections/Path.tsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import pic5 from '../../public/pic5.jpg'
+import Image from 'next/image'
+import style from './Path.module.scss'
 
 const Path = () => {
   return (
-    <div>Path</div>
+    <section className={style.container}>
+      <Image src={pic5} alt='pic4' width={300} height={500} className={style.pic} />
+      <div>
+        <p>
+          En tan solo tres años de trayectoria, ha
+          logrado posicionarse como una de las
+          artistas más relevantes de la escena
+          underground local, llevando su música a
+          ciudades como Buenos Aires, Corrientes,
+          Formosa, Sáenz Peña y Misiones. Su
+          energía en cabina y su técnica depurada
+          le han permitido compartir escenario con
+          referentes como Bruno Brugnoli, Pluj,
+          Franco Musachi, Another, WearsPrada,
+
+          Franzizca y Anita B Queen.
+        </p>
+
+
+      </div>
+    </section>
   )
 }
 
